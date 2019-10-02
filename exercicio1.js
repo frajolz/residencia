9788dffacf1069f3e143d112bd880d7082cde657
frajolz <39355986+frajolz@users.noreply.github.com>
2019-10-02 14:31:23 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,41 @@
+var anosUsuario = 0;
+var mesesUsuario = 8;
+var diasUsuario = 0;
+
+//função para subtrair os dias
+Date.prototype.subtrairDias = function(diasUsuario) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() - diasUsuario);
+    return date;
+}
+
+//função para subtrair os meses
+Date.prototype.subtrairMeses = function(mesesUsuario) {
+    var date = new Date(this.valueOf());
+    date.setMonth(date.getMonth() - mesesUsuario);
+    return date;
+}
+
+//função para subtrair os anos
+Date.prototype.subtrairAnos = function(anosUsuario) {
+    var date = new Date(this.valueOf());
+    date.setFullYear(date.getFullYear() - anosUsuario);
+    return date;
+}
+
+var dataHoje = new Date();
+
+var dataSemDias = (dataHoje.subtrairDias(diasUsuario));//data de hoje sem os dias imputados pelo usuário
+var dataSemMeses = (dataSemDias.subtrairMeses(mesesUsuario));//data sem os dias, subtraindo tambem os meses
+var dataSemAnos = (dataSemMeses.subtrairAnos(anosUsuario)); //data sem os dias e meses, subtraindo os anos (essa é a data desejada para o cálculo)
+
+console.log(dataSemDias);
+console.log(dataSemMeses);
+console.log(dataSemAnos);
+
+dataHoje = new Date();
+
+//cálculo dos dias totais
+var diferencaEmMilissegundos = Math.abs(dataHoje - dataSemAnos);
+var diferencaEmDias = Math.ceil(diferencaEmMilissegundos / (1000 * 60 * 60 * 24)); 
+console.log(diferencaEmDias - 1);
